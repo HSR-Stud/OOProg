817c04d509c342950d41243fda866aca9decc78f
LukiLeu <kingofsteinen@famleu.ch>
2014-08-03 21:11:40 +0200

    Kapitel 6 zämegstungged

--- code/default_parameter_vs_overloading.cpp
+++ code/default_parameter_vs_overloading.cpp
@@ -1,11 +1,9 @@
 // Variante mit Overloading
-// 3 unterschiedliche Funktionen belegen Speicher
-// 3 unterschiedliche Funktionen müssen gewartet werden
+// 3 unterschiedliche Funktionen belegen Speicher und müssen gewartet werden
 void print(int i);
 void print(int i, int width);
 void print(int i, char fillchar, int width);
 
 // Variante mit Default-Parameter
-// Eine einzige Funktion belegt Speicher
-// Nur eine Funktion muss gewartet werden
+// Eine einzige Funktion belegt Speicher und muss gewartet werden
 void print(int i, int width=0, char fillchar=0);
